d36c6ffd93084b853cb21770cfaa7517926219b6
28kayak <kaya.ota@sjsu.edu>
2016-11-06 03:46:13 -0800

    add num1 server

---
+++ Num1/server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by kaya on 11/6/16.
+ */
+var express = require('express');
+
+
+var app = express();
+var port = 8888;
+//if it is Ok to add port number as in check field 
+app.use('/', function(){
+    res.end("AMAZON");
+});git 
